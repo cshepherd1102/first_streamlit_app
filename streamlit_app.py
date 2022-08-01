398283c8a26df7777b39179b18d38fb301f0d920
chris shepherd <cshepherd@navexglobal.com>
2022-08-01 10:08:07 -0400

    Update streamlit_app.py
    added in breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit;
 
 streamlit.title('My parents new wealthy diner')
+streamlit.header('Breakfast menu')
+streamlit.text('Omega 3 and blueberry oatmeal')
+streamlit.text('Kale, Spinach and Rocket smoothie')
+streamlit.text('Hard boiled free range egg')
