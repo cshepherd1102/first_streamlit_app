fc29fd3d53c6e9bb6fe6c5866a1f64dedcdc6ca4
chris shepherd <cshepherd@navexglobal.com>
2022-08-04 12:54:10 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,8 +35,8 @@ try:
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     streamlit.dataframe(fruityvice_normalized)
-  except URLError as e:
-    streamlite.error()
+except URLError as e:
+  streamlite.error()
 
 streamlit.write('The user entered ', fruit_choice)
 
