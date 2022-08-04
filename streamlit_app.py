6cf5b776e06ed430acc10d430c5bb63917350773
chris shepherd <cshepherd@navexglobal.com>
2022-08-04 12:51:31 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,16 +27,20 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
+try:
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Apple')
+if not fruit_choice:
+  streamlit.error("Please select a fruit to get more information.")
+else:
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlite.error()
+
 streamlit.write('The user entered ', fruit_choice)
 
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
-# normalize json
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# normalize data frame
-streamlit.dataframe(fruityvice_normalized)
 
 #troubleshooting
 streamlit.stop()
